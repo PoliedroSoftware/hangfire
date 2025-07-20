ef50b3d49cbb5b971b71882f68901d7efb5daca6
Josue0625 <jcampos@ufpso.edu.co>
2025-07-19 23:53:22 -0500

    restructure project layout and relocate Dockerfile to root

--- PoliedroHangFire/PoliedroHangFire/Infrastructure/External/Billing/Adapters/ClientBilling/ClientService.cs
+++ PoliedroHangFire/PoliedroHangFire/Infrastructure/External/Billing/Adapters/ClientBilling/ClientService.cs
@@ -8,7 +8,7 @@ public class ClientService(HttpClient httpClient) : IClientService
 
     public async Task<List<Domain.ClientBilling.Entities.ClientBilling>> GetClientBillingsAsync()
     {
-        /*_httpClient.DefaultRequestHeaders.Add("X-Environment", "staging-hangfire");
+        _httpClient.DefaultRequestHeaders.Add("X-Environment", "staging-billing");
         var response = await _httpClient.GetAsync("https://wc9oqtphb5.execute-api.us-east-2.amazonaws.com/billing/api/v1/client");
         response.EnsureSuccessStatusCode();
         var content = await response.Content.ReadAsStringAsync();
@@ -16,8 +16,7 @@ public class ClientService(HttpClient httpClient) : IClientService
         return JsonSerializer.Deserialize<List<Domain.ClientBilling.Entities.ClientBilling>>(content, new JsonSerializerOptions
         {
             PropertyNameCaseInsensitive = true
-        });*/
-        return new List<Domain.ClientBilling.Entities.ClientBilling>();
+        });
     }
 
 }
